04ab0bed10bdafc7ac75ca24672f22399c11a5e3
Charles.Cairney <charles.cairney@ecs.co.uk>
2023-10-30 11:24:06 +0000

    Updated app details

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { Sequelize } = require("sequelize");
 
 const PORT = process.env.PORT;
 const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USERNAME;
+const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 
