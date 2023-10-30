c59dad2da9f0c890ce5a3404e8ac1998d20fe225
Charles.Cairney <charles.cairney@ecs.co.uk>
2023-10-30 11:20:22 +0000

    Blog controller created

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { Sequelize } = require("sequelize");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
@@ -15,9 +15,11 @@ const DB_HOST = process.env.DB_HOST;
 // Express Routes Import
 const AuthorizationRoutes = require("./authorization/routes");
 const UserRoutes = require("./users/routes");
+const BlogRoutes = require("./blogs/routes");
 
 // Sequelize model imports
 const UserModel = require("./common/models/User");
+const BlogModel = require("./common/models/Blog");
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -26,10 +28,12 @@ app.use(Express.json());
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
+  port: 5432,
 });
 
 // Initialising the Model on sequelize
 UserModel.initialise(sequelize);
+BlogModel.initialise(sequelize);
 
 // Syncing the models that are defined on sequelize with the tables that already exist
 sequelize.sync()
@@ -38,6 +42,7 @@ sequelize.sync()
     // Attaching the Authentication and User Routes to the app.
     app.use("/", AuthorizationRoutes);
     app.use("/user", UserRoutes);
+    app.use("/blog", BlogRoutes);
 
     app.listen(PORT, () => {
       console.log("Server Listening on PORT:", PORT);
---
+++ blogs/routes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+
+// Middleware Imports
+const isAuthenticatedMiddleware = require("./../common/middlewares/IsAuthenticatedMiddleware");
+const SchemaValidationMiddleware = require("../common/middlewares/SchemaValidationMiddleware");
+const CheckPermissionMiddleware = require("../common/middlewares/CheckPermissionMiddleware");
+
+// Controller Imports
+const BlogController = require("./controllers/blogController");
+
+// JSON Schema Imports for payload verification
+const newBlogPayload = require("./schemas/newBlogPayload");
+const updateBlogPayload = require("./schemas/updateBlogPayload");
+
+router.get("/", [isAuthenticatedMiddleware.check], BlogController.getBlog);
+
+// router.post(
+//   "/",
+//   [
+//     isAuthenticatedMiddleware.check,
+//     SchemaValidationMiddleware.verify(newBlogPayload),
+//   ],
+//   BlogController.newBlog
+// );
+
+router.get(
+  "/all",
+  [isAuthenticatedMiddleware.check],
+  BlogController.getAllBlogs
+);
+
+router.patch(
+  "/update/:blogId",
+  [
+    isAuthenticatedMiddleware.check,
+    SchemaValidationMiddleware.verify(updateBlogPayload),
+  ],
+  BlogController.updateBlog
+);
+
+router.delete(
+  "/:blogId",
+  [isAuthenticatedMiddleware.check],
+  BlogController.deleteBlog
+);
+
+module.exports = router;
