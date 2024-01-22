53f1970d323cf1571d9a9df53dd2ee5a227a457f
CSCairney <charlescairney@yahoo.co.uk>
2024-01-22 15:22:51 +0000

    Add new routes for updating blog rating, upvote counter, and comments

--- blogs/routes.js
+++ blogs/routes.js
@@ -10,7 +10,9 @@ const BlogController = require("./controllers/blogController");
 
 // JSON Schema Imports for payload verification
 const newBlogPayload = require("./schemas/newBlogPayload");
+const newBlogCommentPayload = require("./schemas/newBlogCommentPayload");
 const updateBlogContentPayload = require("./schemas/updateBlogContentPayload");
+const updateBlogUpvoteCounterPayload = require("./schemas/updateBlogUpvoteCounterPayload");
 
 router.get("/", [isAuthenticatedMiddleware.check], BlogController.getBlog);
 
@@ -34,6 +36,30 @@ router.patch(
     CheckPermissionMiddleware.has(roles.ADMIN)
   ],
   BlogController.updateBlog
+);
+
+router.patch(
+  "/update/rating/:blogId",
+  [isAuthenticatedMiddleware.check],
+  BlogController.updateRating
+);
+
+router.patch(
+  "/update/upvote/:blogId",
+  [isAuthenticatedMiddleware.check,
+    SchemaValidationMiddleware.verify(updateBlogUpvoteCounterPayload),
+    CheckPermissionMiddleware.has(roles.USER)],
+  BlogController.updateUpvote
+);
+
+router.patch(
+  "/update/comment/:blogId",
+  [
+    isAuthenticatedMiddleware.check,
+    SchemaValidationMiddleware.verify(newBlogCommentPayload),
+    CheckPermissionMiddleware.has(roles.USER)
+  ],
+  BlogController.updateComment
 );
 
 router.delete(
